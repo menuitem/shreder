850c75b5e39ca088b0a0b601da94f3447d60d042
menuitem <lucjanstepien@gmail.com>
2015-05-27 12:44:02 +0100

    Volume attaching working

--- attachVolume.sh
+++ attachVolume.sh
@@ -2,27 +2,19 @@ attachVolumeToShreder (){ # $1 parameter is a shreder instance Id, $2 is a vloum
 	blockPrefix="xvd"
 	blocksCount=$(aws ec2 describe-instances --instance-ids $1|gawk "/BLOCKDEVICEMAPPINGS.*$blockPrefix/"|wc -l)
 	blockSuffixOrdinal=$((97 + $blocksCount))
-	# blockSuffix="\\$(printf '%03o' "$blockSuffixOrdinal")"
 	blockSuffix=\\$(printf "%o" $blockSuffixOrdinal)
-	blockName="$blockPrefix$blockSuffix"
-	printf "\nblocksCount $blocksCount"
-	printf "\nblockSuffix $blockSuffix"
-	printf "\nblockSuffixOrdinal $blockSuffixOrdinal"
-	printf "\nblockPrefix $blockPrefix"
-	printf "\nblockName $blockName\n"
-
+	blockName="$blockPrefix$(printf $blockSuffix)"
 	blockSuffixOrdinal=96 #reset counter
-	aws ec2 describe-instances --instance-ids $1|gawk "/BLOCKDEVICEMAPPINGS.*$blockName/"
-	while [ "$?" = 0 -a "$blockSuffixOrdinal" -le 121 ]; do
+	isBlockNameInUse=$(aws ec2 describe-instances --instance-ids $1|gawk "/BLOCKDEVICEMAPPINGS.*\/dev\/$blockName/"|wc -l)
+	while [ "$isBlockNameInUse" -eq 1 -a "$blockSuffixOrdinal" -le 121 ]; do #if 0 returned then the blocke name is in use already 
 		let "blockSuffixOrdinal++"
 		blockSuffix=\\$(printf "%o" $blockSuffixOrdinal)
-		blockName="$blockPrefix$blockSuffix"
-		aws ec2 describe-instances --instance-ids $1|gawk "/BLOCKDEVICEMAPPINGS.*$blockName/"
+		blockName="$blockPrefix$(printf $blockSuffix)"
+		isBlockNameInUse=$(aws ec2 describe-instances --instance-ids $1|gawk "/BLOCKDEVICEMAPPINGS.*\/dev\/$blockName/"|wc -l)
 	done
-		printf "\nblockName _$blockName-\n"
-		blockDevice=$(printf "/dev/${blockName}")
-		echo "blockDevice $blockDevice"
-	aws ec2 attach-volume --volume-id $2 --instance-id $1 --device ${blockDevice}
+	blockDevice="/dev/${blockName}"
+	printf "Attaching volume $2 to $1 as $blockDevice device.\n"
+	aws ec2 attach-volume --volume-id $2 --instance-id $1 --device "$blockDevice"
 }
 
 attachVolumeToShreder "i-ca23312c" "vol-108c2b0f"
